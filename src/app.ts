49116d98fbb8e641a9ed04828aa8ba8dabe0452d
JAHID HASAN <developerjahid05@gmail.com>
2025-01-26 20:11:45 +0600

    feat: mongoose setup

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
 import express from 'express'
 import cors from 'cors'
 const app = express()
-const port = process.env.PORT || 5000
+
 app.use(cors())
 app.use(express.json())
 
-app.listen(port, ()=>{
-    console.log(`shopverse server is running on ${port}`)
-})
+
+
+export default app;
